2a353314c8b1307ce0c05be0b53f050772004b84
jvcostaf <joaovcosta220@gmail.com>
2024-08-17 16:22:03 -0300

    Adiciona concorrencia no programa.

--- Lab1/cods-lab1/Atividade 5/program.c
+++ Lab1/cods-lab1/Atividade 5/program.c
@@ -1,24 +1,29 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+
+typedef struct {
+    int* array;
+    int inicio;
+    int fim;
+    int id; // Adiciona um ID para a thread
+} ThreadData;
 
 /*
 Função que cria um vetor de tamanho N, com base no valor escolhido
 */
-int* makeArray(int size){
+int* makeArray(int size) {
+    int* array = (int*)malloc(size * sizeof(int)); // Alocando espaço para o vetor.
 
-    int* array = (int*)malloc(size * sizeof(int)); //Alocando espaco para o meu vetor.
-
-    if(array == NULL){
-        printf("Problemas com a alocacao de memoria");
+    if(array == NULL) {
+        printf("Problemas com a alocação de memória");
         exit(1);
     }
 
-    srand(time(NULL)); //Maneira existente em C para utilizar numeros randomicos.
+    srand(time(NULL)); // Usando números aleatórios
 
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++) {
         array[i] = rand() % 100;
     }
 
@@ -26,52 +31,68 @@ int* makeArray(int size){
 }
 
 /*
-Função que adiciona uma unidade em cada elemento de um vetor passado como argumento.
+Função que será executada por cada thread, somando 1 em cada elemento da sua parte do vetor.
 */
-int* addOneForEachArrayElement(int* array, int size){
-    int* modifiedArray = (int*)malloc(size * sizeof(int)); //Alocando espaco para o vetor alterado.
+void* addOneToEachElement(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
 
-    modifiedArray = array; //Atribuindo ao vetor alterado os valores do vetor original
-
-    if(array == NULL){
-        printf("Problemas com a alocacao de memoria");
-        exit(1);
+    for(int i = data->inicio; i < data->fim; i++) {
+        data->array[i] += 1;
     }
 
-    for(int i = 0; i < size; i++){
-        modifiedArray[i] += 1; //Somando 1 em todos os elementos do vetor alterado
-    }
-
-    return modifiedArray;
+    // Indica que a thread terminou seu processamento
+    printf("Thread %d terminou de processar elementos de %d a %d.\n", data->id, data->inicio, data->fim - 1);
+    return NULL;
 }
 
-int main(void){
+int main(void) {
 
-    int size;
+    int size, num_threads;
     printf("Digite o tamanho do vetor: \n");
     scanf("%d", &size);
-    
+
+    printf("Digite o número de threads: \n");
+    scanf("%d", &num_threads);
+
     int* array = makeArray(size);
 
     printf("Vetor de %d posições:\n", size);
     printf("[ ");
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
-    printf("]");
+    printf("]\n");
 
-    printf("\n");
+    pthread_t threads[num_threads];
+    ThreadData thread_data[num_threads];
+    int parte = size / num_threads;
 
-    int* modifiedArray = addOneForEachArrayElement(array, size);
+    // Criando threads para processar o vetor
+    for(int i = 0; i < num_threads; i++) {
+        thread_data[i].array = array;
+        thread_data[i].inicio = i * parte;
+        thread_data[i].fim = (i == num_threads - 1) ? size : (i + 1) * parte;
+        thread_data[i].id = i; // Define o ID da thread
 
-    printf("Vetor alterado:\n");
-    printf("[ ");
-    for(int i = 0; i < size; i++){
-        printf("%d ", modifiedArray[i]);
+        if(pthread_create(&threads[i], NULL, addOneToEachElement, &thread_data[i]) != 0) {
+            printf("Erro ao criar a thread %d.\n", i);
+            return 1;
+        }
     }
-    printf("]");
 
-    printf("\n");
+    // Espera todas as threads terminarem
+    for(int i = 0; i < num_threads; i++) {
+        if(pthread_join(threads[i], NULL)){
+            printf("--ERRO: pthread_join() da thread %d\n", i); 
+        }
+    }
+
+    printf("Vetor de %d posições alterado:\n", size);
+    printf("[ ");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("]\n");
 
     free(array);
 
