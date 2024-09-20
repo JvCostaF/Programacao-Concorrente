c2e9195e110ec28988d7c8da28ba77a5803e6271
jvcostaf <joaovcosta220@gmail.com>
2024-09-19 22:31:29 -0300

    Corrige problema com a implementação

--- Lista1/piBailey.c
+++ Lista1/piBailey.c
@@ -2,15 +2,24 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+    Não perca tempo compilando no Windows, tive sérios problemas por lá.
+    Para compilar no linux, a lib math.h não é linkada com outras básicas,
+    então precisamos dizer que ela será usada no processo de build com o 
+    gcc da seguinte maneira
+
+    $ gcc -o pi piBailey.c -lm
+*/
+
 long double piBBP(int n){
 
     long double pi = 0.0;
 
     for (int i = 0; i < n; i++) {
         long double termo = (4.0 / (8*i + 1)) -
-                            (2.0 / (8*i + 4)) -
-                            (1.0 / (8*i + 5)) -
-                            (1.0 / (8*i + 6));
+                        (2.0 / (8*i + 4)) -
+                        (1.0 / (8*i + 5)) -
+                        (1.0 / (8*i + 6));
         pi += termo / powl(16, i);
     }
 
@@ -27,7 +36,7 @@ int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
 
     long double pi = piBBP(n);
-    printf("Valor aproximado de pi com %d termos: %.9Lf\n", n, pi);
+    printf("Valor aproximado de pi com %d termos: %.14Lf\n", n, pi);
 
     return 0;
 }
