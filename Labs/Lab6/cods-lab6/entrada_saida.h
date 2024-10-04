146d4514a5a83c2b071601a42f1a34c2724e6821
jvcostaf <joaovcosta220@gmail.com>
2024-10-03 22:22:27 -0300

    Modularizando a solucao da atividade

---
+++ Labs/Lab6/cods-lab6/entrada_saida.h
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+//lock de exclusao mutua
+pthread_mutex_t mutex;
+//variaveis condicionais para controlar a ordem que os leitores e escritores executam
+pthread_cond_t cond_ler, cond_escrever;
+//variaceis globais para controlar a quantidade de leitores e escritores
+int leitores = 0; int escritores = 0; int querendoEscrever = 0; int leitoresAguardando = 0;
+
+void EntraLeitura() {
+    pthread_mutex_lock(&mutex);
+
+    leitoresAguardando++;
+
+    //verifica se temos escritores escrevendo ou querendo escrever
+    while(escritores > 0 || querendoEscrever > 0) { //alguem ja escrevendo ou alguem querendo escrever
+        printf("Leitor: Existem escritores trabalhando e ou querendo escrever, aguarde!\n");
+        printf("Leitores aguardando: %d\n", leitoresAguardando);
+        pthread_cond_wait(&cond_ler, &mutex);
+    }
+
+    leitoresAguardando--;
+
+    leitores++;
+    printf("Chegou um leitor\n");
+    pthread_mutex_unlock(&mutex);
+}
+
+void SaiLeitura() {
+    pthread_mutex_lock(&mutex);
+
+    printf("Leitor saindo.\n");
+    printf("Leitores aguardando: %d\n", leitoresAguardando);
+    leitores--;
+    pthread_cond_broadcast(&cond_escrever);
+    pthread_cond_broadcast(&cond_ler);
+    pthread_mutex_unlock(&mutex);
+}
+
+void EntraEscrita() {
+    pthread_mutex_lock(&mutex);
+
+    querendoEscrever++;
+
+    while(leitores > 0 || escritores > 0) {
+        printf("Escritor: Existem %d escritor(es) ou %d leitor(es) trabalhando e %d escritores esperando, aguarde!\n", escritores, leitores, querendoEscrever);
+        pthread_cond_wait(&cond_escrever, &mutex);
+    }
+
+    querendoEscrever--;
+
+    escritores++;
+    printf("Chegou um escritor\n");
+    pthread_mutex_unlock(&mutex);
+}
+
+void SaiEscrita() {
+    pthread_mutex_lock(&mutex);
+
+    printf("Escritor saindo.\n");
+    printf("Escritores aguardando: %d\n", querendoEscrever);
+    escritores--;
+
+    pthread_cond_broadcast(&cond_ler);
+    pthread_cond_broadcast(&cond_escrever);
+    pthread_mutex_unlock(&mutex);
+}
--- Labs/Lab6/cods-lab6/read_write.c
+++ Labs/Lab6/cods-lab6/read_write.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "list_int.h"
 #include <pthread.h>
+#include "entrada_saida.h"
 #include "timer.h"
 
 #define QTDE_OPS 100 //quantidade de operacoes sobre a lista (insercao, remocao, consulta)
@@ -12,71 +13,7 @@
 struct list_node_s* head_p = NULL; 
 //qtde de threads no programa
 int nthreads;
-//lock de exclusao mutua
-pthread_mutex_t mutex;
-//variaveis condicionais para controlar a ordem que os leitores e escritores executam
-pthread_cond_t cond_ler, cond_escrever;
-//variaceis globais para controlar a quantidade de leitores e escritores
-int leitores = 0; int escritores = 0; int querendoEscrever = 0; int leitoresAguardando = 0;
 
-void EntraLeitura() {
-    pthread_mutex_lock(&mutex);
-
-    leitoresAguardando++;
-
-    //verifica se temos escritores escrevendo ou querendo escrever
-    while(escritores > 0 || querendoEscrever > 0) { //alguem ja escrevendo ou alguem querendo escrever
-        printf("Leitor: Existem escritores trabalhando e ou querendo escrever, aguarde!\n");
-        printf("Leitores aguardando: %d\n", leitoresAguardando);
-        pthread_cond_wait(&cond_ler, &mutex);
-    }
-
-    leitoresAguardando--;
-
-    leitores++;
-    printf("Chegou um leitor\n");
-    pthread_mutex_unlock(&mutex);
-}
-
-void SaiLeitura() {
-    pthread_mutex_lock(&mutex);
-
-    printf("Leitor saindo.\n");
-    printf("Leitores aguardando: %d\n", leitoresAguardando);
-    leitores--;
-    pthread_cond_broadcast(&cond_escrever);
-    pthread_cond_broadcast(&cond_ler);
-    pthread_mutex_unlock(&mutex);
-}
-
-void EntraEscrita() {
-    pthread_mutex_lock(&mutex);
-
-    querendoEscrever++;
-
-    while(leitores > 0 || escritores > 0) {
-        printf("Escritor: Existem %d escritor(es) ou %d leitor(es) trabalhando e %d escritores esperando, aguarde!\n", escritores, leitores, querendoEscrever);
-        pthread_cond_wait(&cond_escrever, &mutex);
-    }
-
-    querendoEscrever--;
-
-    escritores++;
-    printf("Chegou um escritor\n");
-    pthread_mutex_unlock(&mutex);
-}
-
-void SaiEscrita() {
-    pthread_mutex_lock(&mutex);
-
-    printf("Escritor saindo.\n");
-    printf("Escritores aguardando: %d\n", querendoEscrever);
-    escritores--;
-
-    pthread_cond_broadcast(&cond_ler);
-    pthread_cond_broadcast(&cond_escrever);
-    pthread_mutex_unlock(&mutex);
-}
 
 //tarefa das threads
 void* tarefa(void* arg) {
