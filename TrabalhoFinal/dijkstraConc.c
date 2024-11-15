c3f553d8c6bb9a3b91011478b7b2c0a682a62144
jvcostaf <joaovcosta220@gmail.com>
2024-11-14 22:28:25 -0300

    feat: Creates vertice and matriz structs

--- TrabalhoFinal/dijkstraConc.c
+++ TrabalhoFinal/dijkstraConc.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+typedef struct {
+    int linhas;
+    int colunas;
+    int tamanho;
+    float *dados;
+} MatrizDeAdjacencias;
+
+typedef struct {
+    int distRaiz;
+    char antecessor;
+    int jaVisitado;
+    char id;
+} Vertice;
+
