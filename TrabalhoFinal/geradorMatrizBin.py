b3aa1ceafdc4b029b325265d243a17d9ad9bcbff
jvcostaf <joaovcosta220@gmail.com>
2024-11-24 22:17:55 -0300

    feat: Altera gerador de Grafos
    Alteramos a forma como estávamos gerando os grafos para testar os
    algoritmos. Agora utilizamos um método, da mesma lib NetworkX, que
    garante gerar mais rápido. Além disso fizemos uma versão que escreve
    em um arquivo binário pensando em otimizar a geração dos grafos.

--- TrabalhoFinal/geradorMatrizBin.py
+++ TrabalhoFinal/geradorMatrizBin.py
@@ -1,23 +1,27 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import struct
 
 def gerar_grafo(n, p, peso_maximo=10):
     # Gera um grafo aleatório G(n, p)
     G = nx.fast_gnp_random_graph(n, p)
-    
+
     # Cria uma matriz de adjacência com pesos
     matriz_adjacencia = np.zeros((n, n), dtype=int)
-    
+
     for (u, v) in G.edges():
         peso = np.random.randint(1, peso_maximo + 1)  # Peso aleatório entre 1 e peso_maximo
         matriz_adjacencia[u][v] = peso
         matriz_adjacencia[v][u] = peso  # Para grafo não direcionado
-    
+
     return G, matriz_adjacencia
 
-def salvar_matriz_em_txt(matriz, nome_arquivo):
-    np.savetxt(nome_arquivo, matriz, fmt='%d')
+def salvar_matriz_em_binario(matriz, nome_arquivo):
+    with open(nome_arquivo, 'wb') as arquivo:
+        for row in matriz:
+            for valor in row:
+                arquivo.write(struct.pack('f', valor))
 
 # Parâmetros do grafo
 n =  # Número de vértices
@@ -27,7 +31,7 @@ p =  # Probabilidade de arestas
 G, matriz = gerar_grafo(n, p)
 
 # Salva a matriz em um arquivo .txt
-salvar_matriz_em_txt(matriz, '')
+salvar_matriz_em_binario(matriz, '')
 
 # Printa o grafo usando o pypltu
 # nx.draw(G, with_labels=True, node_color='lightblue', edge_color='gray')
